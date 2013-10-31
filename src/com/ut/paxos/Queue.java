578bfc875128805c3e51055527661383e0831a61
Ankit Goyal <ankit3goyal@gmail.com>
2013-10-31 17:32:25 -0500

    adding inital paxos code

---
+++ src/com/ut/paxos/Queue.java
@@ -0,0 +1,19 @@
+package com.ut.paxos;
+
+import java.util.LinkedList;
+
+public class Queue<T> {
+    LinkedList<T> ll = new LinkedList<T>();
+
+    public synchronized void enqueue(T obj){
+        ll.add(obj);
+        notify();
+    }
+
+    public synchronized T bdequeue(){
+       while (ll.size() == 0) {
+           try { wait(); } catch (InterruptedException e) {}
+       }
+       return ll.removeFirst();
+   }
+}
